17badbfade28b5b742c8dead1f4ac602eaf1874d
hazartilirot <hazartilirot@gmail.com>
2022-01-13 15:24:59 +0200

    A composite of structs

--- structs/main.go
+++ structs/main.go
@@ -2,26 +2,52 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo // We may just use a type alone omitting a property.
 }
 
 func main() {
 	//implicit assignment
-	alex := person{"Alex", "Anderson"}
+	alex := person{"Alex", "Anderson", contactInfo{"alex@exampl.ecom", 12345}}
 
 	//explicit assignment
 	john := person{
 		firstName: "John",
 		lastName:  "Doe",
+		contact: contactInfo{
+			email:   "john@example.com",
+			zipCode: 12345,
+		},
 	}
 	// announcing a variable with a zero value (empty string)
 	var jane person
 	jane.lastName = "Jane"
 	jane.lastName = "Doe"
+	jane.contact.email = "janedoe@example.com"
+	jane.contact.zipCode = 12345
 
-	fmt.Println(alex)
-	fmt.Println(john)
-	fmt.Println(jane)
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim@example.com",
+			zipCode: 12345,
+		},
+	}
+
+	alex.print()
+	john.print()
+	jane.print()
+	jim.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
